f10bc79ea291bc55118a4264b22c63d65e5c374c
André Restivo <andre.restivo@gmail.com>
2023-06-02 20:15:06 +0100

    feat: files can return contents without whitespace

--- src/files.ts
+++ src/files.ts
@@ -3,8 +3,9 @@ import * as fs from 'fs-extra'
 import { globSync } from 'glob'
 
 export class Files {
-  public readFile (file: string) {
-    return fs.readFileSync(file, 'utf-8')
+  public readFile (file: string, whitespace: boolean) {
+    if (whitespace) return fs.readFileSync(file, 'utf-8').replace(/\s{2,}/g, ' ')
+    else return fs.readFileSync(file, 'utf-8')
   }  
 
   public files (folder: string, extension: string) {
